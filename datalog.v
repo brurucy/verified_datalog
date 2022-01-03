97b3932983a6cf10ff3bfd4a57e6535274623730
brurucy <rucy.carneiro@pipedrive.com>
2022-01-03 21:36:25 +0200

    Doesn't work properly 💩

--- datalog.v
+++ datalog.v
@@ -1,11 +1,48 @@
 (* This is a tremendously simple certified implementation of a unipredicate datalog engine. *)
 
 Require Import String.
-Require Import Datalog.Maps.
 Require Import List.
+Require Import Bool.Bool.
 Import ListNotations.
 
 Module Datalog.
+
+(* eqb_string and the following three theorems are not my work. *)
+Definition eqb_string (x y : string) : bool := if string_dec x y then true else false.
+
+Theorem eqb_string_refl : forall s : string, true = eqb_string s s.
+Proof.
+  intros s. unfold eqb_string.
+  destruct (string_dec s s) as [Hs_eq | Hs_not_eq].
+  - reflexivity.
+  - destruct Hs_not_eq. reflexivity.
+Qed.
+
+Theorem eqb_string_true_iff : forall x y : string,
+  eqb_string x y = true <-> x = y.
+Proof.
+  intros x y.
+  unfold eqb_string.
+  destruct (string_dec x y) as [Hs_eq | Hs_not_eq].
+  - rewrite Hs_eq. split. reflexivity. reflexivity.
+  - split.
+    + intros contra. discriminate contra.
+    + intros H. exfalso. apply Hs_not_eq. apply H.
+Qed.
+
+Theorem eqb_string_false_iff : forall x y : string,
+  eqb_string x y = false <-> ~(x = y).
+Proof.
+  intros x y.
+  rewrite <- eqb_string_true_iff.
+  rewrite not_true_iff_false.
+  reflexivity.
+Qed.
+
+(* Datalog is a decidable subset of prolog, whose core use is to take
+   Atoms as an input, and materialize the logical consequences of rules
+   applied to them.
+ *)
 
 (* First we need to define the building blocks of datalog, Terms. *)
 Inductive tm : Type :=
@@ -28,6 +65,11 @@ Definition eqb_term (lterm : tm) (rterm: tm) : bool :=
   | tm_var lvalue, tm_var rvalue => eqb_string lvalue rvalue
   | tm_const lvalue, tm_const rvalue => eqb_string lvalue rvalue
   end.
+
+Lemma eqb_term_refl : forall t t' :  tm,
+    eqb_term t t' = true -> eqb_term t' t = true.
+Proof.
+Admitted.
 
 Compute eqb_term (tm_const "Professor") (tm_var "Professor").
 Compute eqb_term (tm_var "Professor") (tm_const "Professor").
@@ -54,6 +96,11 @@ Fixpoint eqb_term_list (l1 l2 : list tm) : bool :=
   | h :: l, h' :: r => if eqb_term h h' then eqb_term_list l r else false
   | _, _ => false                                                                 
   end.
+
+Lemma eqb_term_list_refl : forall t t' :  tm,
+    eqb_term t t' = true -> eqb_term t' t = true.
+Proof.
+Admitted.
 
 Definition eqb_atom (latom : atom) (ratom : atom) : bool :=
   match latom, ratom with                                       
@@ -205,9 +252,9 @@ Compute substitution_step example_kb (atom_regular "ancestor" [ tm_var "X" ; tm_
 
  *)
 Fixpoint eval_atom (kb : KnowledgeBase) (a1 : atom) (ls acc : list substitution) : list substitution :=
-  match ls with
-  | nil => acc
-  | h :: l => match substitute_atom a1 h with
+  match ls, acc with
+  | nil, _ => acc
+  | h :: l, _ => match substitute_atom a1 h with
             (* If the atom is not ground, then there is a substitution  *)
             | Some a => let new_substitution := substitution_step kb a [] in
                        eval_atom kb a1 l (acc ++ new_substitution)
@@ -229,7 +276,98 @@ Definition example_kb_two := [ example_ground_atom_five ; example_ground_atom_si
 
 Compute eval_atom example_kb_two example_regular_atom_four example_substitution_list [].
 
-(* A rule has a head and a body. the head is a single non-ground atom, and the body is a list of atoms *)
+(* A rule is a clause that has a body, a list of atoms, and a head, a single regular atom.
+
+   ancestor(X, Y), ancestor(Y, Z) -> ancestor(X, Z)
+
+   It reads as "if ancestor(X, Y) and ancestor(Y, Z) are true, then  so does ancestor(X, Z) "
+
+   the left-hand side is called the body, and the right-hand side is the head of the rule.
+
+   This is the last part of datalog syntax.
+ *)
+Inductive cl : Type :=
+  | cl_rule : atom -> list atom -> cl
+.
+
+Definition example_rule_body := [atom_regular "ancestor" [ tm_var "X" ; tm_var "Y"] ; atom_regular "ancestor" [ tm_var "Y" ; tm_var "Z"] ].
+Definition example_rule_head := atom_regular "ancestor" [ tm_var "X" ; tm_var "Z"].
+
+Definition example_rule := cl_rule example_rule_head example_rule_body.
+
+(*
+  In eval_body all we do is walk through the rule's body in order to get all substitutions.
+
+  Given the following list of atoms:
+
+  ancestor(X, Y), ancestor(Y, Z)
+
+  And knowledge base:
+
+  [ (adam, jumala) ; (vanasarvik, jumala) ; (eve, adam) ; (jumala, cthulu) ]
+
+  We should get the following long list of substitutions:
+
+  [ [ (X, adam) ; (X, vanasarvik) ; (X, eve) ; (X, jumala) ; (Y, jumala) ; (Y, adam) ; (Y, cthulu) ] ;
+  [ (Y, adam) ; (Y, vanasarvik) ; (Y, eve) ; (Y, jumala) ; (Z, jumala) ; (Z, adam) ; (Z, cthulu) ] ]
+
+ *)
+Fixpoint eval_body (kb : KnowledgeBase) (l1 : list atom) (ls : list substitution) : list substitution :=
+  match l1 with
+  | nil => ls  
+  | h :: l => match ls with
+            | nil => let atom_genesis := substitution_step kb h [] in
+                    eval_body kb l atom_genesis
+            | _ => let atom_evaluation := eval_atom kb h ls [] in
+                  eval_body kb l (ls ++ atom_evaluation)
+            end
+  end.
+
+Definition example_ground_atom_seven := atom_ground "ancestor" [ tm_const "jumala" ; tm_const "cthulu" ].
+
+Definition example_kb_three := [ example_ground_atom_two ; example_ground_atom_three ; example_ground_atom_four ; example_ground_atom_seven ].
+
+Compute example_kb_three.
+
+Compute eval_body example_kb_three [atom_regular "ancestor" [ tm_var "X" ; tm_var "Y" ] ; atom_regular "ancestor" [ tm_var "Y" ; tm_var "Z" ]] [].
+
+(*
+
+  Evaluating a rule is quite straightforward.
+  
+  We will evaluate the body of the rule, and then attempt to use the substitutions to generate new atoms.
+
+  The function substitute_head does nothing special.
+
+  It is the same as substitute_atom, except that instead of returning an option, and acting on a signle substitution
+  it takes in a list of substitutions and applies them to a single atom.
+
+  For instance, applying the 
+
+ *)
+
+Fixpoint substitute_head (head : atom) (substitutions: list substitution) ( acc : KnowledgeBase ): KnowledgeBase :=
+  match substitutions with
+  | nil => acc
+  | h :: l => match substitute_atom head h with
+            | Some fresh_atom => substitute_head head l (acc ++ [fresh_atom])
+            | None => substitute_head head l (acc)
+            end
+  end.
+
+Definition previous_substitutions := eval_body example_kb_three [atom_regular "ancestor" [ tm_var "X" ; tm_var "Y" ] ; atom_regular "ancestor" [ tm_var "Y" ; tm_var "Z" ]] [].
+
+Compute previous_substitutions.
 
 
-(* This is the classic datalog example rule. *)
+
+Compute substitute_head example_rule_head previous_substitutions [].
+
+Definition eval_rule (kb : KnowledgeBase) (r : cl) : KnowledgeBase :=
+  match r with
+  | cl_rule head body =>
+      let substitutions := eval_body kb body [] in
+      substitute_head head substitutions []
+  end.
+
+Compute eval_rule example_kb_three example_rule.
